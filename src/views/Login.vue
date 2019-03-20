2ee86e18b32441b63d1642c3e9b2010993ad43fb
Parashara <parashara@gmail.com>
2019-03-19 23:49:24 -0700

    added script src to login component and works but is janky

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,9 +12,9 @@
           <ul v-if="errors" class="error-messages">
             <li v-for="(v, k) in errors" :key="k">{{ k }} {{ v | error }}</li>
           </ul>
-          <form v-on:submit.prevent="onSubmit(email, password);">
+          <form id="myForm" v-on:submit.prevent="onSubmit(email, password, castle_client_id);">
             <fieldset class="form-group">
-              <input
+              <input id="email"
                 class="form-control form-control-lg"
                 type="text"
                 v-model="email"
@@ -22,11 +22,19 @@
               />
             </fieldset>
             <fieldset class="form-group">
-              <input
+              <input id="password"
                 class="form-control form-control-lg"
                 type="password"
                 v-model="password"
                 placeholder="Password"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input id="castle"
+                class="form-control form-control-lg"
+                type="text"
+                v-model="castle_client_id"
+                placeholder="don't input here it will automatically be done with castle"
               />
             </fieldset>
             <button class="btn btn-lg btn-primary pull-xs-right">
@@ -43,7 +51,21 @@
 import { mapState } from "vuex";
 import { LOGIN } from "@/store/actions.type";
 
+
+
 export default {
+    mounted() {
+    let recaptchaScript = document.createElement('script')
+    // castle CDN -- put your app id after the js? to replace YOUR-APP-ID
+    recaptchaScript.setAttribute('src', 'https://d2t77mnxyo7adj.cloudfront.net/v1/c.js?YOUR-APP-ID')
+    document.head.appendChild(recaptchaScript)
+    // _castle('identify', 'e325bcdd10ac') 
+    var clientId = _castle('getClientId');
+    console.log(clientId)
+    document.getElementById('castle').value = clientId
+    document.getElementById('email').value = "hey@hey.com"
+    document.getElementById('password').value = "ascend1008"
+  },
   name: "RwvLogin",
   data() {
     return {
@@ -52,9 +74,10 @@ export default {
     };
   },
   methods: {
-    onSubmit(email, password) {
+    onSubmit(email, password, castle_client_id) {
+      console.log(email, password, castle_client_id)
       this.$store
-        .dispatch(LOGIN, { email, password })
+        .dispatch(LOGIN, { email, password, castle_client_id })
         .then(() => this.$router.push({ name: "home" }));
     }
   },
