fd44909a1f37a22ff48245a645cde28282bdb387
Parashara <parashara@gmail.com>
2019-03-20 10:31:08 -0700

    working app but need to copy paste clientId on form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,41 +5,40 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sign in</h1>
           <p class="text-xs-center">
-            <router-link :to="{ name: 'register' }">
-              Need an account?
-            </router-link>
+            <router-link :to="{ name: 'register' }">Need an account?</router-link>
           </p>
           <ul v-if="errors" class="error-messages">
             <li v-for="(v, k) in errors" :key="k">{{ k }} {{ v | error }}</li>
           </ul>
           <form id="myForm" v-on:submit.prevent="onSubmit(email, password, castle_client_id);">
             <fieldset class="form-group">
-              <input id="email"
+              <input
+                id="email"
                 class="form-control form-control-lg"
                 type="text"
                 v-model="email"
                 placeholder="Email"
-              />
+              >
             </fieldset>
             <fieldset class="form-group">
-              <input id="password"
+              <input
+                id="password"
                 class="form-control form-control-lg"
                 type="password"
                 v-model="password"
                 placeholder="Password"
-              />
+              >
             </fieldset>
             <fieldset class="form-group">
-              <input id="castle"
+              <input
+                id="castle"
                 class="form-control form-control-lg"
                 type="text"
                 v-model="castle_client_id"
                 placeholder="don't input here it will automatically be done with castle"
-              />
+              >
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">
-              Sign in
-            </button>
+            <button class="btn btn-lg btn-primary pull-xs-right">Sign in</button>
           </form>
         </div>
       </div>
@@ -50,21 +49,21 @@
 <script>
 import { mapState } from "vuex";
 import { LOGIN } from "@/store/actions.type";
-
-
-
 export default {
-    mounted() {
-    let recaptchaScript = document.createElement('script')
+  mounted() {
+    let recaptchaScript = document.createElement("script");
     // castle CDN -- put your app id after the js? to replace YOUR-APP-ID
-    recaptchaScript.setAttribute('src', 'https://d2t77mnxyo7adj.cloudfront.net/v1/c.js?YOUR-APP-ID')
-    document.head.appendChild(recaptchaScript)
-    // _castle('identify', 'e325bcdd10ac') 
-    var clientId = _castle('getClientId');
-    console.log(clientId)
-    document.getElementById('castle').value = clientId
-    document.getElementById('email').value = "hey@hey.com"
-    document.getElementById('password').value = "ascend1008"
+    recaptchaScript.setAttribute(
+      "src",
+      "https://d2t77mnxyo7adj.cloudfront.net/v1/c.js?YOUR-APP-ID"
+    );
+    document.head.appendChild(recaptchaScript);
+    _castle('identify', 'e325bcdd10ad')
+    var clientId = _castle("getClientId");
+    console.log(clientId);
+    document.getElementById("castle").value = clientId;
+    document.getElementById("email").value = "hey@hey.com";
+    document.getElementById("password").value = "ascend1008";
   },
   name: "RwvLogin",
   data() {
@@ -75,7 +74,7 @@ export default {
   },
   methods: {
     onSubmit(email, password, castle_client_id) {
-      console.log(email, password, castle_client_id)
+      console.log(email, password, castle_client_id);
       this.$store
         .dispatch(LOGIN, { email, password, castle_client_id })
         .then(() => this.$router.push({ name: "home" }));
